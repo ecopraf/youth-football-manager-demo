d16b09b8086017099d5e3f8e7da29d93c3b3037b
ecopraf <coppola.raffaele@gmail.com>
2026-06-30 13:56:00 +0200

    fix: rimuovi 'Caricamento...' visibile durante banner benvenuto demo

--- demo/frontend/src/build-info.js
+++ demo/frontend/src/build-info.js
@@ -1,13 +1,13 @@
 // Auto-generated build info
 // SW Version: v3.14
-// Build Number: 5
-// Build ID: v3.14.5
-// Date: 30/06/2026, 13:52:12
+// Build Number: 6
+// Build ID: v3.14.6
+// Date: 30/06/2026, 13:55:41
 export const BUILD_INFO = {
-  id: 'v3.14.5',
+  id: 'v3.14.6',
   version: 'v3.14',
-  buildNumber: 5,
-  date: '2026-06-30T11:52:12.943Z',
-  buildDate: '30/06/2026, 13:52:12'
+  buildNumber: 6,
+  date: '2026-06-30T11:55:41.636Z',
+  buildDate: '30/06/2026, 13:55:41'
 };
 export const SW_VERSION = 'v3.14';
